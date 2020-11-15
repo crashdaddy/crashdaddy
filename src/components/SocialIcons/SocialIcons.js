f9c3277817dc2f9f2e303da54d3a8fb5d83c5178
crashdaddy <kennethscoggins@mail.com>
2020-11-15 17:30:41 -0600

    added hover effect to social icons

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -8,11 +8,11 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 function SocialIcons() {
  
   return <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',width:'30%',marginTop:'20px',marginLeft:'auto',marginRight:'auto'}}>
-      <a href="https://www.reddit.com/user/crashdaddy" target="blank"><RedditIcon style={{fill:'grey',width:'32px',height:'32px'}} /></a>
-      <a href="https://www.facebook.com/eddie.salazar.526875" target="blank"><FacebookIcon style={{fill:'grey',width:'32px',height:'32px'}} /></a>
-      <a href="https://twitter.com/crashdaddy4" target="blank"><TwitterIcon style={{fill:'grey',width:'32px',height:'32px'}} /></a>
-      <a href="https://www.linkedin.com/in/kenneth-scoggins-464a48197/" target="blank"><LinkedInIcon style={{fill:'grey',width:'32px',height:'32px'}} /></a>
-      <a href="https://github.com/crashdaddy" target="blank"><GitHubIcon style={{fill:'grey',width:'32px',height:'32px'}} /></a>
+      <a href="https://www.reddit.com/user/crashdaddy" target="blank"><RedditIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
+      <a href="https://www.facebook.com/eddie.salazar.526875" target="blank"><FacebookIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
+      <a href="https://twitter.com/crashdaddy4" target="blank"><TwitterIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
+      <a href="https://www.linkedin.com/in/kenneth-scoggins-464a48197/" target="blank"><LinkedInIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
+      <a href="https://github.com/crashdaddy" target="blank"><GitHubIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
   </div>
 }
  

8072b4ea741678d1e27d83a3af019c6ac7a48884
crashdaddy <kennethscoggins@mail.com>
2021-08-25 14:51:28 -0500

    removed FB social icon

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -9,7 +9,7 @@ function SocialIcons() {
  
   return <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',width:'30%',marginTop:'20px',marginLeft:'auto',marginRight:'auto'}}>
       <a href="https://www.reddit.com/user/crashdaddy" target="blank"><RedditIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
-      <a href="https://www.facebook.com/eddie.salazar.526875" target="blank"><FacebookIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
+      {/* <a href="https://www.facebook.com/eddie.salazar.526875" target="blank"><FacebookIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a> */}
       <a href="https://twitter.com/crashdaddy4" target="blank"><TwitterIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
       <a href="https://www.linkedin.com/in/kenny-scoggins-9b292b21a/" target="blank"><LinkedInIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
       <a href="https://github.com/crashdaddy" target="blank"><GitHubIcon className="socialIcons" style={{width:'32px',height:'32px'}} /></a>
